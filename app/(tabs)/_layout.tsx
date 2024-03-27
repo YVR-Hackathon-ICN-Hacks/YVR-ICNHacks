aa238221523f94e8bd191a86f48b4fe0533c45bb
ys421 <ychung47@my.bcit.ca>
2024-03-27 05:33:17 -0700

    [YVR-Client-34] Implement Camera Feature

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,6 +6,7 @@ import { Pressable } from 'react-native';
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import CameraScreen from './camera'; // Import the CameraScreen component
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -54,6 +55,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
+<Tabs.Screen
+  name="camera"
+  options={{
+    title: 'Camera',
+    tabBarIcon: ({ color }) => <TabBarIcon name="camera" color={color} />,
+  }}
+/>
     </Tabs>
   );
 }
