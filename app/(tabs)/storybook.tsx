ef71f960fa5d796c73721b94d4a43be7c246195b
gdcho <gcho13@my.bcit.ca>
2024-03-31 19:33:30 -0700

    [yvr-client-46] Story book screen and components

---
+++ app/(tabs)/storybook.tsx
@@ -0,0 +1,33 @@
+import { View, StyleSheet } from "react-native";
+import Clickable from "@/components/Clickable";
+
+export default function StoryBook() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.separator} />
+      <Clickable href="/storybook/refresh" iconName="refresh" text="Refresh" />
+      <Clickable
+        href="/storybook/authentication"
+        iconName="home"
+        text="Authentication"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    rowGap: 20,
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  pressable: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
