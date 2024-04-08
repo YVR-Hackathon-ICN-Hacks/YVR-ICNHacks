d6d746b6c66d63e57f5d97e9d8e602ce679b5e77
gdcho <gcho13@my.bcit.ca>
2024-04-07 22:20:15 -0700

    [Test] Test data

--- app/(tabs)/storybook.tsx
+++ app/(tabs)/storybook.tsx
@@ -1,52 +1,59 @@
 import { View, StyleSheet } from "react-native";
 import Clickable from "@/components/Clickable";
+import { ScrollView } from "react-native";
 
 export default function StoryBook() {
   return (
-    <View style={styles.container}>
-      <View style={styles.separator} />
-      <Clickable href="/storybook/refresh" iconName="refresh" text="Refresh" />
-      <Clickable
-        href="/storybook/authentication"
-        iconName="home"
-        text="Authentication"
-      />
-      <Clickable
-        href="/storybook/(ministack)"
-        iconName="plus"
-        text="Top Stack Navigator"
-         />
-      <Clickable 
-        href="/storybook/google_map"
-        iconName = "map"
-        text="Google Map" 
-      />
-      <Clickable
-        href="/storybook/push_notification"
-        iconName="bell"
-        text="Push Notification"
-      />
-      <Clickable 
-        href="/storybook/fetchTest"
-        iconName = "coffee"
-        text="fetchTest" 
-      />
-      <Clickable
-        href="/storybook/(thermal_data)"
-        iconName="plus"
-        text="Thermal Data"
-      />
-      <Clickable
-        href="/storybook/(sunlight_exposure)"
-        iconName="plus"
-        text="Sunlight Exposure"
-      />
-      <Clickable
-        href="/storybook/(hvac_performance)"
-        iconName="plus"
-        text="HVAC Performance"
-      />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.separator} />
+        <Clickable
+          href="/storybook/refresh"
+          iconName="refresh"
+          text="Refresh"
+        />
+        <Clickable
+          href="/storybook/authentication"
+          iconName="home"
+          text="Authentication"
+        />
+        <Clickable
+          href="/storybook/(ministack)"
+          iconName="plus"
+          text="Top Stack Navigator"
+        />
+        <Clickable
+          href="/storybook/google_map"
+          iconName="map"
+          text="Google Map"
+        />
+        <Clickable
+          href="/storybook/push_notification"
+          iconName="bell"
+          text="Push Notification"
+        />
+        <Clickable
+          href="/storybook/fetchTest"
+          iconName="coffee"
+          text="fetchTest"
+        />
+        <Clickable
+          href="/storybook/(thermal_data)"
+          iconName="plus"
+          text="Thermal Data"
+        />
+        <Clickable
+          href="/storybook/(sunlight_exposure)"
+          iconName="plus"
+          text="Sunlight Exposure"
+        />
+        <Clickable
+          href="/storybook/(hvac_performance)"
+          iconName="plus"
+          text="HVAC Performance"
+        />
+      </View>
+    </ScrollView>
   );
 }
 
@@ -57,7 +64,7 @@ const styles = StyleSheet.create({
     rowGap: 20,
   },
   separator: {
-    marginVertical: 30,
+    marginVertical: 10,
     height: 1,
     width: "80%",
   },
